5adef473ef7eafb04bf3b15534af9cbef115ea5f
bromium <levdenisov@gmail.com>
2015-03-15 14:24:00 +0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,18 @@
 
 $('#submit').click(function(){
 
-$.post("//formspree.io/m17021963@gmail.com", $(".contact-form").serialize(),  function() {   
- $('#success').html("Thank you! Your message was sent!");
+//$.post("//formspree.io/m17021963@gmail.com", $(".contact-form").serialize(),  function() {   
+// $('#success').html("Thank you! Your message was sent!");
+//});
+$.ajax({
+    url: "//formspree.io/m17021963@gmail.com", 
+    method: "POST",
+    data: $(".contact-form").serialize(),
+    dataType: "json",
+	success: function(response) {
+	 console.log(response);
+	 $('#success').html(response);
+	}
 });
 return false;
 
