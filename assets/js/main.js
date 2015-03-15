c374f703131dd018279f44cd496985db276fe898
bromium <levdenisov@gmail.com>
2015-03-15 14:06:17 +0300

    Update main.js
    remove response in function

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 $('#submit').click(function(){
 
-$.post("//formspree.io/m17021963@gmail.com", $(".contact-form").serialize(),  function(response) {   
+$.post("//formspree.io/m17021963@gmail.com", $(".contact-form").serialize(),  function() {   
  $('#success').html("Thank you! Your message was sent!");
 });
 return false;
