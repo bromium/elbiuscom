bebc452a57e91c6bef3461d1e8d35a40427355cd
bromium <levdenisov@gmail.com>
2015-03-15 14:03:24 +0300

    Update main.js
    Add response message

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 $('#submit').click(function(){
 
 $.post("//formspree.io/m17021963@gmail.com", $(".contact-form").serialize(),  function(response) {   
- $('#success').html(response);
+ $('#success').html("Thank you! Your message was sent!");
 });
 return false;
 
